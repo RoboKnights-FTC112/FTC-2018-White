f36481d56bcb2b24d187a1143ed774b46af1f642
John O <JohnOBrien@Johns-MacBook-Pro.local>
2018-11-06 08:52:27 -0500

    Updated autonomous code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousLeft.java
@@ -8,15 +8,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous
 
 public class AutonomousLeft extends LinearOpMode{
-    public DcMotor arm;
     public DcMotor rearLeft;
     public DcMotor rearRight;
     public DcMotor frontLeft;
     public DcMotor frontRight;
     
-    public Servo plowOne;
+    public Servo armOne;
     public Servo plowTwo;
-    public DcMotor plowMotor;
+    public DcMotor armMotor;
     
     public void runOpMode() {
 
@@ -31,54 +30,75 @@ public class AutonomousLeft extends LinearOpMode{
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         
-        //plow servos
-        plowOne = hardwareMap.servo.get("plowOne");
-        plowTwo = hardwareMap.servo.get("plowTwo");
-        
-        plowMotor=hardwareMap.dcMotor.get("plowMotor");
+        //arm servos
+        armOne = hardwareMap.servo.get("plowOne");
         
         //arm motor
-        DcMotor arm = hardwareMap.get(DcMotor.class, "arm");
+        armMotor=hardwareMap.dcMotor.get("plowMotor");
+
 
         if(opModeIsActive()){
-            plowOne.setPosition(.5);
+            armOne.setPosition(.5);
             driveForward(0.4,1500);
             moveArm(true);
             sleep(900);
-            plowOne.setPosition(1);
+            armOne.setPosition(1);
             moveArm(false);
             turnRight(0.4,700);
             driveForward(1,3000);
         }
     }
-    
-    public void driveForward(double speed, double time){
+
+
+    public void driveForward(double speed, int time){
         rearLeft.setPower(-speed);
         rearRight.setPower(speed);
         frontLeft.setPower(-speed);
         frontRight.setPower(speed);
-        sleep((long)time);
+        sleep(time);
         rearLeft.setPower(0);
         rearRight.setPower(0);
         frontLeft.setPower(0);
         frontRight.setPower(0);
-        
+    }
+    public void driveBackwards(double speed, int time){
+        rearLeft.setPower(speed);
+        rearRight.setPower(-speed);
+        frontLeft.setPower(speed);
+        frontRight.setPower(-speed);
+        sleep(time);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
     }
     public void moveArm(boolean pos){
         if(pos){
-            plowMotor.setPower(-0.6);
+            armMotor.setPower(-0.6);
             sleep(400);
         } else {
-            plowMotor.setPower(0.6);
+            armMotor.setPower(0.6);
             sleep(650);
         }
-        plowMotor.setPower(0);
+        armMotor.setPower(0);
     }
     public void turnRight(double speed, int time){
         rearLeft.setPower(-speed);
         rearRight.setPower(-speed);
         frontLeft.setPower(-speed);
         frontRight.setPower(-speed);
+        sleep(time);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+    }
+
+    public void turnLeft(double speed, int time){
+        rearLeft.setPower(speed);
+        rearRight.setPower(speed);
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
         sleep(time);
         rearLeft.setPower(0);
         rearRight.setPower(0);
